b5436317d97f91c49b0a875dd6bd7bda3f17fb1c
Farooq Hidayat <farooq@Farooq-MacBook-Pro.local>
2021-03-21 13:30:56 +0500

    pagination added

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, ListCreateAPIView
+from rest_framework.pagination import PageNumberPagination
 from .serializers import TestSerializer
 from .models import TestModel
+
+class CustomPagination(PageNumberPagination):
+    pagination_class = PageNumberPagination
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10 
 
 class Test(ListAPIView):
 
     serializer_class = TestSerializer
     queryset = TestModel.objects.all()
+    pagination_class = CustomPagination
 
 class TestCreate(ListCreateAPIView):
     
