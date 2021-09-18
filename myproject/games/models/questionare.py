7339f8f85314c506aca9b802a4118db7181ec634
Farooq Hidayat <farooq.hidayat@themarketiq.com>
2021-09-19 04:05:36 +0500

    game migration files added

--- myproject/games/models/questionare.py
+++ myproject/games/models/questionare.py
@@ -7,14 +7,15 @@ from user_registration.models import UserModel
 class Questions(models.Model):
 
     question = models.CharField(max_length=200)
-    correct_answer = models.CharField(max_length=50)
-    user = models.ForeignKey(UserModel,on_delete=models.CASCADE)
 
 
 class QuestionsOptions(models.Model):
 
-    option_1 = models.CharField(max_length=50)
-    option_2 = models.CharField(max_length=50)
-    option_3 = models.CharField(max_length=50)
-    option_4 = models.CharField(max_length=50)
-    question = models.ForeignKey(Questions,on_delete=models.CASCADE)
+    option_text = models.CharField(max_length=50)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+
+class UserQuestions(models.Model):
+
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    correct_answer = models.ForeignKey(QuestionsOptions, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
