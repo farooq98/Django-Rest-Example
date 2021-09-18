e460c91ee95bba0d04483b10d7859215c05c0484
Muhammad Salman <1996.muhammadsalman@gmail.com>
2021-09-18 13:39:52 +0500

    function call fix for verification email

--- myproject/user_registration/models/custom_user_model.py
+++ myproject/user_registration/models/custom_user_model.py
@@ -61,14 +61,14 @@ class UserModel(AbstractBaseUser, PermissionsMixin):
     def change_password(self):
         if self.is_active:
             self.verification_code = generate_random_code()
-            send_verfication_email(self.email, self.verification_code, purpose="password reset")
+            send_verification_email(self.email, self.verification_code, purpose="password reset")
             self.verification_code_timeout = timezone.now() + timedelta(minutes=10)
             self.save()
 
     def send_email(self):
         if not self.is_active:
             self.verification_code = generate_random_code()
-            send_verfication_email(self.email, self.verification_code)
+            send_verification_email(self.email, self.verification_code)
             self.verification_code_timeout = timezone.now() + timedelta(minutes=10)
 
     def validate_timeout(self, code):
