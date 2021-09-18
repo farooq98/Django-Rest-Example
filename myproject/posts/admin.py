605639c03d7094716de8b3c05609e619e18da36a
Farooq Hidayat <farooq.hidayat@themarketiq.com>
2021-09-19 00:39:39 +0500

    admin changes

--- myproject/posts/admin.py
+++ myproject/posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post, Comment
 
 # Register your models here.
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    can_delete = False
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Comment, CommentInline)
