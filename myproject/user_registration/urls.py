314842e9e739496390539d5f58145b1be83b15ff
Farooq Hidayat <farooq.hidayat@themarketiq.com>
2021-09-18 16:32:27 +0500

    resend code api attached

--- myproject/user_registration/urls.py
+++ myproject/user_registration/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import CreateUser, ActivateUser, LoginUser, LogoutView, CheckAuth, ForgetPassword, RequestForgetPassword, \
-    CreateWorkSpace,AddMembersWorkSpace
+    CreateWorkSpace, AddMembersWorkSpace, ResendVerificationCode
 
 urlpatterns = [
     path('signup/', CreateUser.as_view()),
     path('login/', LoginUser.as_view()),
     path('verify/', ActivateUser.as_view()),
+    path('resend/code/', ResendVerificationCode.as_view()),
     path('logout/', LogoutView.as_view()),
     path('check/auth/', CheckAuth.as_view()),
     path('forget/password/', ForgetPassword.as_view()),
