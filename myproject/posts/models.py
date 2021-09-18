04517bf3c3c572bd689fb4e7eb6cfebb8bbf85d1
Farooq Hidayat <farooq.hidayat@themarketiq.com>
2021-09-19 02:14:27 +0500

    fixing models

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -28,7 +28,7 @@ class Comment(models.Model):
 
 class Like(models.Model):
     
-    post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name="likes")
     user = models.ForeignKey(UserModel, on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
