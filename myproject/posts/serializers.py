087f3847197aaf362e9952c28695e8f3f001bfaf
Farooq Hidayat <farooq.hidayat@themarketiq.com>
2021-09-19 01:45:06 +0500

    fixing serializers

--- myproject/posts/serializers.py
+++ myproject/posts/serializers.py
@@ -3,15 +3,16 @@ from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
 
-    created_by = serializers.ReadOnlyField(source='user.username')
+    created_by = serializers.ReadOnlyField(source='user.name')
+    email = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         model = Post
-        fields = ['id', 'created_by', 'image_url', 'content', 'created_at', 'updated_at']
+        fields = ['id', 'created_by', 'image_url', 'content', 'likes', 'created_at', 'updated_at', 'email']
 
 class CommentSerializer(serializers.ModelSerializer):
 
-    created_by = serializers.ReadOnlyField(source='user.username')
+    created_by = serializers.ReadOnlyField(source='user.name')
 
     class Meta:
         model = Post
