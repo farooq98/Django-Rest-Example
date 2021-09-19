2bf868c099c7e3b733f336ba20e7d523f8389adf
Farooq Hidayat <farooq.hidayat@themarketiq.com>
2021-09-19 13:58:37 +0500

    adding quiz answer model in admin

--- myproject/games/admin.py
+++ myproject/games/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Questions,UserQuestions,QuestionsOptions
+from .models import Questions,UserQuestions,QuestionsOptions, QuizAnswer
 # Register your models here.
 
 # admin.site.register(Post)
 admin.site.register(Questions)
 admin.site.register(UserQuestions)
 admin.site.register(QuestionsOptions)
+admin.site.register(QuizAnswer)
