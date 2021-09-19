5ede1bdb446e4af6bf92302d52fa56fbc84a4c6d
Farooq Hidayat <farooq.hidayat@themarketiq.com>
2021-09-19 10:28:57 +0500

    view quiz users created

--- myproject/games/urls.py
+++ myproject/games/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-# from .views import 
+from .views import GetAllUsersWithQuiz
 
 urlpatterns = [
-    
+    path('users/quiz/', GetAllUsersWithQuiz.as_view())
 ]
--- myproject/games/views.py
+++ myproject/games/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from core.authentication import PrivateAPI
+from rest_framework.response import Response
+from rest_framework import status
+from user_registration.models import UserWorkSpaceRelationTable
+from .models import UserQuestions
 
 # Create your views here.
 
+class GetAllUsersWithQuiz(PrivateAPI):
+    
+    def get(self, request):
+
+        try:
+            UserWorkSpaceRelationTable.objects.get(
+                workspace__id = request.GET.get('workspace_id'), 
+                user = request.user
+            )
+        except UserWorkSpaceRelationTable.DoesNotExist:
+            return Response({
+                "status": False,
+                "message": "Invalid workspace"
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        workspace_users = [obj.user for obj in UserWorkSpaceRelationTable.objects.filter(
+            workspace__id = request.GET.get('workspace_id')
+        )]
+
+        return Response([{
+            'user_id': usr.id,
+            'email': usr.email
+        } for usr in UserQuestions.objects.filter(user__in=workspace_users)], status=status.HTTP_200_OK)
 
