96d6e21a771ea9b79b93859aa80bee868d0d454d
Farooq Hidayat <farooq.hidayat@themarketiq.com>
2021-09-19 11:00:36 +0500

    commit

--- myproject/games/admin.py
+++ myproject/games/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Questions, QuestionsOptions, UserQuestions
 # Register your models here.
+
+admin.site.register(Questions)
+admin.site.register(QuestionsOptions)
+admin.site.register(UserQuestions)
