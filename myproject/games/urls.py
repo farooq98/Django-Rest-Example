54a985fd74868e90eee904409685a2c09bbbe352
Farooq Hidayat <farooq.hidayat@themarketiq.com>
2021-09-19 12:02:19 +0500

    get post api created

--- myproject/games/urls.py
+++ myproject/games/urls.py
@@ -4,5 +4,5 @@ from .views import GetAllUsersWithQuiz,QuestionView,QuizAnswer
 urlpatterns = [
     path('users/quiz/', GetAllUsersWithQuiz.as_view()),
     path('questionare/', QuestionView.as_view()),
-    path('answer_quiz/', QuizAnswer.as_view()),
+    path('answer/quiz/', QuizAnswer.as_view()),
 ]
