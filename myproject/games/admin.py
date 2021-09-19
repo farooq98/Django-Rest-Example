9d631f8ac5cda4610289110617541d1c7e3a1908
Muhammad Salman <1996.muhammadsalman@gmail.com>
2021-09-19 10:57:17 +0500

    games admin.py updated

--- myproject/games/admin.py
+++ myproject/games/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Questions,UserQuestions,QuestionsOptions
 # Register your models here.
+
+# admin.site.register(Post)
+admin.site.register(Questions)
+admin.site.register(UserQuestions)
+admin.site.register(QuestionsOptions)
