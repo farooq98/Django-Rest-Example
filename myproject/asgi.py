c25e2deb90dab2fa113d37cbf3997c0b941d4836
Farooq Hidayat <farooq.hidayat@themarketiq.com>
2021-10-09 14:17:02 +0500

    asgi changes

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -5,9 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.conf.urls import url
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 django_asgi_app = get_asgi_application()
 
 from social.routing import websockets_urls
