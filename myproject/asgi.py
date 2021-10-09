17d34f9899cf50945172f0683aaa3ec671531905
Farooq Hidayat <farooq.hidayat@themarketiq.com>
2021-10-09 16:11:38 +0500

    created new app chat

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -8,7 +8,8 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 django_asgi_app = get_asgi_application()
 
-from social.routing import websockets_urls
+# from social.routing import websockets_urls
+import chat.routing
 
 application = ProtocolTypeRouter({
     # Django's ASGI application to handle traditional HTTP requests
@@ -17,7 +18,8 @@ application = ProtocolTypeRouter({
     # WebSocket chat handler
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            websockets_urls
+            # websockets_urls
+            chat.routing.websocket_urlpatterns
         )
     ),
 })
--- myproject/routing.py
+++ myproject/routing.py
@@ -1,11 +1,13 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from social.routing import websockets_urls
+# from social.routing import websockets_urls
+import chat.routing
 
 application = ProtocolTypeRouter({
     'websockets': AuthMiddlewareStack(
         URLRouter(
-            websockets_urls
+            # websockets_urls
+            chat.routing.websocket_urlpatterns
         )
     )
 })
