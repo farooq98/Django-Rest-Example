82d437c8f81ef762e0eec237ef2ac84fb5834a6e
Farooq Hidayat <farooq.hidayat@themarketiq.com>
2021-10-09 14:35:09 +0500

    some more heroku deployement changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,40 +1,18 @@
 from pathlib import Path
 import os
+import json
 
-try:
-    from . import environment
-except ImportError:
-    DEBUG = True 
-    HOST = 'LOCALHOST'
-    SECRET_KEY = "mysecretkey"
-    ALLOWED_HOSTS = ['*']
-    TIME_ZONE = "UTC"
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    EMAIL_HOST_USER = "admin@myproject.com"
-    STORAGE_LINK = "your storage link"
-    APP_ID = "your app id"
-    IREBASE_PROJECT_EMAIL = "youremail"
-    FIREBASE_PROJECT_PASS = "yourpassword"
-    FIREBASE_CONFIG = {
-        "apiKey": "your api key",
-        "authDomain": "your auth domain",
-        "projectId": "your project id",
-        "databaseURL":"your database url",
-        "storageBucket": "your storage bucket",
-        "messagingSenderId": "your messaging sender id",
-        "appId": "your app id"
-    }
-else:
-    DEBUG = True if environment.ENV in ["DEV", "STAGE"] else False
-    HOST = environment.HOST
-    SECRET_KEY = environment.SECRET_KEY
-    ALLOWED_HOSTS = environment.ALLOWED_HOSTS
-    TIME_ZONE = environment.TIME_ZONE
-    STORAGE_LINK = environment.STORAGE_LINK
-    APP_ID = environment.APP_ID
-    FIREBASE_PROJECT_EMAIL = environment.FIREBASE_PROJECT_EMAIL
-    FIREBASE_PROJECT_PASS = environment.FIREBASE_PROJECT_PASS
-    FIREBASE_CONFIG = environment.FIREBASE_CONFIG
+if not os.environ.get('HOST') == 'HEROKU':
+    DEBUG = True if os.environ.get("ENV") in ["DEV", "STAGE"] else False
+    HOST = os.environ.get("HOST")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    ALLOWED_HOSTS = json.loads(os.environ.get("ALLOWED_HOSTS"))
+    TIME_ZONE = os.environ.get("TIME_ZONE")
+    STORAGE_LINK = os.environ.get("STORAGE_LINK")
+    APP_ID = os.environ.get("APP_ID")
+    FIREBASE_PROJECT_EMAIL = os.environ.get("FIREBASE_PROJECT_EMAIL")
+    FIREBASE_PROJECT_PASS = os.environ.get("FIREBASE_PROJECT_PASS")
+    FIREBASE_CONFIG = json.loads(os.environ.get("FIREBASE_CONFIG"))
     if DEBUG:
         EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
         EMAIL_HOST_USER = "admin@myproject.com"
@@ -42,9 +20,56 @@ else:
         EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
         EMAIL_HOST = 'smtp.gmail.com'
         EMAIL_PORT = 587
-        EMAIL_HOST_USER = environment.EMAIL
-        EMAIL_HOST_PASSWORD = environment.PASSWORD
+        EMAIL_HOST_USER = os.environ.get('EMAIL')
+        EMAIL_HOST_PASSWORD = os.environ.get('PASSWORD')
         EMAIL_USE_TLS = True
+
+else:
+
+    try:
+        from . import environment
+    except ImportError:
+        DEBUG = True 
+        HOST = 'LOCALHOST'
+        SECRET_KEY = "mysecretkey"
+        ALLOWED_HOSTS = ['*']
+        TIME_ZONE = "UTC"
+        EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+        EMAIL_HOST_USER = "admin@myproject.com"
+        STORAGE_LINK = "your storage link"
+        APP_ID = "your app id"
+        IREBASE_PROJECT_EMAIL = "youremail"
+        FIREBASE_PROJECT_PASS = "yourpassword"
+        FIREBASE_CONFIG = {
+            "apiKey": "your api key",
+            "authDomain": "your auth domain",
+            "projectId": "your project id",
+            "databaseURL":"your database url",
+            "storageBucket": "your storage bucket",
+            "messagingSenderId": "your messaging sender id",
+            "appId": "your app id"
+        }
+    else:
+        DEBUG = True if environment.ENV in ["DEV", "STAGE"] else False
+        HOST = environment.HOST
+        SECRET_KEY = environment.SECRET_KEY
+        ALLOWED_HOSTS = environment.ALLOWED_HOSTS
+        TIME_ZONE = environment.TIME_ZONE
+        STORAGE_LINK = environment.STORAGE_LINK
+        APP_ID = environment.APP_ID
+        FIREBASE_PROJECT_EMAIL = environment.FIREBASE_PROJECT_EMAIL
+        FIREBASE_PROJECT_PASS = environment.FIREBASE_PROJECT_PASS
+        FIREBASE_CONFIG = environment.FIREBASE_CONFIG
+        if DEBUG:
+            EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+            EMAIL_HOST_USER = "admin@myproject.com"
+        else:
+            EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+            EMAIL_HOST = 'smtp.gmail.com'
+            EMAIL_PORT = 587
+            EMAIL_HOST_USER = environment.EMAIL
+            EMAIL_HOST_PASSWORD = environment.PASSWORD
+            EMAIL_USE_TLS = True
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -143,7 +168,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if HOST != 'HEROKU':
+if not os.environ.get('HOST'):
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
